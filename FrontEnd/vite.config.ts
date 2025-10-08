9a5dadc7fa155e52b231ff82c2f3bd9fe4914d9b
cleverson-programmer <cleverson.github@gmail.com>
2025-10-08 16:51:37 -0300

    fix: correção tailwind.config

---
+++ FrontEnd/vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: ".", // raiz é a própria pasta frontend
+  server: {
+    host: "localhost",
+    port: 5173
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  build: {
+    outDir: "dist", // saída dentro da pasta frontend
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom", "react-router-dom"],
+          "redux-vendor": ["@reduxjs/toolkit", "react-redux"],
+          "ui-vendor": [
+            "lucide-react",
+            "@radix-ui/react-dialog",
+            "@radix-ui/react-tabs"
+          ]
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
+});
