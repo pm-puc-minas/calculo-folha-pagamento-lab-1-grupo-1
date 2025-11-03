8a848d6d9764ae76ba0472ef692f57e0b07f6270
Yuri Viana <ycviana@sga.pucminas.br>
2025-11-03 11:38:06 -0300

    feat(user): Cria UserRequestDTO para entrada de dados e validação

---
+++ folha-pagamento/src/main/java/com/payroll/dtos/user/UserRequestDTO.java
@@ -0,0 +1,73 @@
+package com.payroll.dtos.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UserRequestDTO {
+    private Long id;
+
+    @NotBlank(message = "O nome de usuário é obrigatório.")
+    @Size(min = 3, max = 50, message = "O nome de usuário deve ter entre 3 e 50 caracteres.")
+    private String username;
+
+    @NotBlank(message = "O e-mail é obrigatório.")
+    @Email(message = "O e-mail deve ser válido.")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória.")
+    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres.")
+    private String password;
+
+    private String role;
+
+    public UserRequestDTO() {}
+    
+    public UserRequestDTO(Long id, String username, String email, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
