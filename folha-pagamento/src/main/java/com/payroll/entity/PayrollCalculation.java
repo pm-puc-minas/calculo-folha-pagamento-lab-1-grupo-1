b18728b739637fdda833875fc6c4b27698354437
Murilo <muriloduartemouradealmeida@gmail.com>
2025-11-05 02:28:03 -0300

    Finalizacoes e correcoes

--- folha-pagamento/src/main/java/com/payroll/entity/PayrollCalculation.java
+++ folha-pagamento/src/main/java/com/payroll/entity/PayrollCalculation.java
@@ -1,12 +1,14 @@
 package com.payroll.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference; 
-import com.fasterxml.jackson.annotation.JsonFormat; 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "payroll_calculations")
